498751c4972dd15e19da88a2fc72d69955643ba4
Juarezoss <127915761+Juarezoss@users.noreply.github.com>
2024-05-11 16:22:13 -0600

    tablas modificadas

--- REDB.sql
+++ REDB.sql
@@ -13,8 +13,8 @@ create TABLE admins(
 	name Varchar (200) not null,
 	middlename VARCHAR(200) not null,
 	lastname  VARCHAR(200) not null,
-	position VARCHAR(200) not null
-	
+	position VARCHAR(200) not null,
+	PASSWORD VARCHAR(500) NOT NULL
 );
 
 describe admins;
@@ -25,11 +25,19 @@ create TABLE buildings(
 	id INT (11) unsigned auto_increment not NULL primary KEY,
 	NAME VARCHAR(200) not NULL,
 	adress VARCHAR(200) not NULL,
-	LOCATION VARCHAR(200) not NULL,
+	LOCATION JSON not NULL,
 	picture VARCHAR(200) not NULL DEFAULT "generic.jpg",
+	PIC_CAPTION VARCHAR(200) NOT NULL,
+	DESCRIPTION VARCHAR (200),
 	admin_id  INT(11) unsigned not NULL,
+	CREATE_AT TIMESTAMP,
 	
 	FOREIGN key (admin_id) REFERENCES admins(id)
 );
 
 DESCRIBE buildings
+DROP TABLE admins;
+
+drop TABLE buildings;
+DROP TABLE admins;
+
